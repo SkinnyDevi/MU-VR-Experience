01f7758a3d5cbb88f9fdc4a02ea0fa7f6c5bb305
felixmv <felixmv379@gmail.com>
2021-11-09 18:22:25 +0000

    Frontend Initial Commit

---
+++ frontend/MU-VR-Experience/Assets/VRAssets/KeyboardMovementControls.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovementControls : MonoBehaviour
+{
+	public CharacterController controller;
+
+	public float speedMovement = 12f;
+	public float gravity = -9.81f;
+	public Transform groundCheck;
+	public float groundDistance = 0.4f;
+	public LayerMask groundMask;
+
+	Vector3 velocity;
+	bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+		if (isGrounded && velocity.y < 0) velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Vector3 movementArrow = transform.right * x + transform.forward * z;
+
+		controller.Move(movementArrow * speedMovement * Time.deltaTime);
+
+		velocity.y += gravity * Time.deltaTime;
+
+		controller.Move(velocity * Time.deltaTime);
+    }
+}

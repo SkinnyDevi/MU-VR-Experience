0d8a89fed543ca5ac004ae7a0a785cc7d637684a
SkinnyDevi <xthesmartdartx@gmail.com>
2021-11-24 19:27:47 +0000

    Added option to change to VR player in settings

---
+++ frontend/MU-VR-Experience/Assets/VRAssets/Scripts/PlayerVRHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVRHandler : MonoBehaviour
+{
+    public enum PlayerType {Mouse, VR};
+	public GameObject OVRPlayerObject, MousePlayerObject;
+	public static PlayerType CurrentPlayerType = PlayerType.Mouse;
+
+	Vector3 lobbyPlayerStartingCoords = new Vector3(-1451.133f, -286.0826f, 178.8876f);
+
+	public void ChangePlayerType(PlayerType type)
+	{
+		CurrentPlayerType = type;
+		TransitionToPlayerType();
+	}
+
+	void TransitionToPlayerType()
+	{
+		if (CurrentPlayerType == PlayerType.VR)
+		{
+			MousePlayerObject.SetActive(false);
+			OVRPlayerObject.SetActive(true);
+		}
+		else
+		{
+			OVRPlayerObject.SetActive(false);
+			MousePlayerObject.SetActive(true);
+		}
+	}
+}
--- frontend/MU-VR-Experience/Assets/VRAssets/Scripts/SettingsMenu.cs
+++ frontend/MU-VR-Experience/Assets/VRAssets/Scripts/SettingsMenu.cs
@@ -1,14 +1,20 @@
 using System.Runtime.InteropServices;
 using UnityEngine;
 
+using TMPro;
+
 public class SettingsMenu : MonoBehaviour
 {
 	public GameObject SettingsMenuObject;
 	public static bool InSettings;
 	public GameObject Crosshair;
 
+	bool vrOptionChanged = false;
+	PlayerVRHandler playerHandler;
+
     void Start()
     {
+		playerHandler = GameObject.FindObjectOfType<PlayerVRHandler>();
 		SettingsMenuObject.SetActive(false);
     }
 
@@ -28,7 +34,6 @@ public class SettingsMenu : MonoBehaviour
 				}
 			}
 		}
-			
     }
 
 	public void PauseGame()
@@ -43,6 +48,18 @@ public class SettingsMenu : MonoBehaviour
 	{
 		SetState(Crosshair, true);
 		SetState(SettingsMenuObject, false);
+		if (vrOptionChanged)
+		{
+			playerHandler.ChangePlayerType(PlayerVRHandler.PlayerType.VR);
+			//TODO: call player handler -> do transition -> switch camera to vr player
+			// --------------------------> change to vr
+		}
+		else
+		{
+			playerHandler.ChangePlayerType(PlayerVRHandler.PlayerType.Mouse);
+			//TODO: call player handler -> do transition ----------> switch camera to mouse player
+			// --------------------------> change to mouse player
+		}
 		Time.timeScale = 1f;
 		InSettings = false;
 	}
@@ -50,6 +67,21 @@ public class SettingsMenu : MonoBehaviour
 	public void SetState(GameObject g, bool state)
 	{
 		g.SetActive(state);
+	}
+
+	public void VRToggleHandler()
+	{
+		TMP_Text vrStateText = gameObject.transform.Find("Settings Menu/Canvas/Pause Menu/Enable VR Checkbox/Toggle/Label").GetComponent<TMP_Text>();
+		if (vrStateText.text.Equals("Disabled"))
+		{
+			vrStateText.text = "Enabled";
+			vrOptionChanged = true;
+		}
+		else
+		{
+			vrStateText.text = "Disabled";
+				vrOptionChanged = false;
+		}
 	}
 
 	[DllImport("__Internal")]

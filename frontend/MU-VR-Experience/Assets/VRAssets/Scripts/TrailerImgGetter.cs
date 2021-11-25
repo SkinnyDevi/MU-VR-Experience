26d448a56138f40d423e8412a5c2222e202b7fca
SkinnyDevi <xthesmartdartx@gmail.com>
2021-11-25 19:20:06 +0000

    Added base frames and image to billboard

---
+++ frontend/MU-VR-Experience/Assets/VRAssets/Scripts/TrailerImgGetter.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerImgGetter : MonoBehaviour
+{
+    //TODO: Grab image from DB and place inside 2D Sprite inside frame
+}

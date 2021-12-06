9ee1fc7093cabb13f198d7667b5ad9608270a3ab
SkinnyDevi <xthesmartdartx@gmail.com>
2021-12-06 01:20:47 +0000

    Limited number of clip frames in wall display

--- frontend/MU-VR-Experience/Assets/VRAssets/Scripts/ImageFramesSpawner.cs
+++ frontend/MU-VR-Experience/Assets/VRAssets/Scripts/ImageFramesSpawner.cs
@@ -39,16 +39,20 @@ public class ImageFramesSpawner : MonoBehaviour
 			{
 				JSONNode response = JSON.Parse(getImages.downloadHandler.text);
 				int baseXCoord = -195;
+				int frameCounts = 0;
 				foreach (JSONNode clip in response)
 				{
-					ClipFrameObject.GetComponent<TrailerImgGetter>().Base64ToSprite(clip["clip_trailer_img"]);
-					ClipFrameObject.transform.position = new Vector3(baseXCoord, 46.17f, -11.03f);
-					ClipFrameObject.transform.Find("Canvas/ClipName").GetComponent<TMP_Text>().text = clip["clip_name"];
-					baseXCoord += 2;
-					GameObject newFrame = Instantiate(ClipFrameObject, gameObject.transform);
-					newFrame.transform.Find("Canvas/Rating Text").GetComponent<RatingPercentageHandler>().SetRequestInfo(clip["clip_id"], userDataObj.GetToken());
-					EnterButtonObject.name = "Enter-" + clip["clip_id"];
-					Instantiate(EnterButtonObject, newFrame.transform);
+					if (frameCounts < 9) {
+						ClipFrameObject.GetComponent<TrailerImgGetter>().Base64ToSprite(clip["clip_trailer_img"]);
+						ClipFrameObject.transform.position = new Vector3(baseXCoord, 46.17f, -11.03f);
+						ClipFrameObject.transform.Find("Canvas/ClipName").GetComponent<TMP_Text>().text = clip["clip_name"];
+						baseXCoord += 2;
+						GameObject newFrame = Instantiate(ClipFrameObject, gameObject.transform);
+						newFrame.transform.Find("Canvas/Rating Text").GetComponent<RatingPercentageHandler>().SetRequestInfo(clip["clip_id"], userDataObj.GetToken());
+						EnterButtonObject.name = "Enter-" + clip["clip_id"];
+						Instantiate(EnterButtonObject, newFrame.transform);
+						frameCounts++;
+					}
 				}
 			}
 		}

dce0440fcd92f5f8ee9cee2de43f5b0bc9d67ca5
SkinnyDevi <xthesmartdartx@gmail.com>
2022-01-14 17:29:18 +0000

    Added test for Change in Player Type to Mouse

--- frontend/MU-VR-Experience/Assets/Tests/PlayMode/TestPlayerTypeChange.cs
+++ frontend/MU-VR-Experience/Assets/Tests/PlayMode/TestPlayerTypeChange.cs
@@ -9,7 +9,7 @@ using UnityEngine.TestTools;
 public class TestPlayerTypeChange
 {
     [UnityTest]
-    public IEnumerator PlayerTypeChanges()
+    public IEnumerator PlayerTypeChangeVR()
     {
 		var gameObject = new GameObject();
 		var playerHandler = gameObject.AddComponent<PlayerVRHandler>();
@@ -26,5 +26,26 @@ public class TestPlayerTypeChange
 		Assert.AreEqual(PlayerVRHandler.PlayerType.VR, PlayerVRHandler.CurrentPlayerType);
 		Assert.AreEqual(false, simulatedMouse.activeSelf);
 		Assert.AreEqual(true, simulatedVR.activeSelf);
+    }
+
+	[UnityTest]
+	public IEnumerator PlayerTypeChangeMouse()
+    {
+		var gameObject = new GameObject();
+		var playerHandler = gameObject.AddComponent<PlayerVRHandler>();
+		var simulatedVR = new GameObject();
+		var simulatedMouse = new GameObject();
+
+		playerHandler.OVRPlayerObject = simulatedVR;
+		playerHandler.MousePlayerObject = simulatedMouse;
+
+		playerHandler.ChangePlayerType(PlayerVRHandler.PlayerType.VR);
+		yield return new WaitForSeconds(0.1f);
+		playerHandler.ChangePlayerType(PlayerVRHandler.PlayerType.Mouse);
+		yield return new WaitForSeconds(0.1f);
+
+		Assert.AreEqual(PlayerVRHandler.PlayerType.Mouse, PlayerVRHandler.CurrentPlayerType);
+		Assert.AreEqual(true, simulatedMouse.activeSelf);
+		Assert.AreEqual(false, simulatedVR.activeSelf);
     }
 }
